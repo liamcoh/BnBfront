71e96067fa42526398d6e6520bed5d5f4b7cc1c1
LCmanutd@gmail.com <LCmanutd@gmail.com>
2021-05-20 02:42:32 +0300

    blabla

--- platform/src/Components/Header.js
+++ platform/src/Components/Header.js
@@ -4,14 +4,17 @@ import headerStyles from './Header.module.css';
 function Header () {
 
     return (
-        <div className={headerStyles.flexBox}>
-            <div className={headerStyles.flexBox2}>
-                <img className={headerStyles.img} src={logo} alt=""/> 
-                <div className={headerStyles.text}>בומבום פאו</div>
+        <div>
+            <div className={headerStyles.flexBox}>
+                <div className={headerStyles.flexBox2}>
+                    <img className={headerStyles.img} src={logo} alt=""/> 
+                    <div className={headerStyles.text}>בומבום פאו</div>
+                </div>
+                <button className={headerStyles.button}>הוסף מקום אירוח</button>
             </div>
-            <button className={headerStyles.button}>הוסף מקום אירוח</button>
-        </div>
 
+
+        </div>
     )
 }
 
