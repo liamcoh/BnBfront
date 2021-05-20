6ca7e531de27548ef027ddb8a31ee08c0c84e2f7
LCmanutd@gmail.com <LCmanutd@gmail.com>
2021-05-20 06:29:03 +0300

    hara

--- platform/src/Components/Header.js
+++ platform/src/Components/Header.js
@@ -2,10 +2,65 @@ import logo from '../Assets/SnowHouse.gif'
 import headerStyles from './Header.module.css';
 import React, { useState } from 'react';
 import { Button } from '@material-ui/core';
+import axios from "axios";
+
+async function handleSubmit(address1,num1,name1,phone1){
+//     {
+//         "address": {
+//             "id": 10710,
+//             "country": "Qatar",
+//             "city": "South Desireberg",
+//             "street": "Franklyn Parkway",
+//             "apt": 5320,
+//             "lat": 30.4125135027,
+//             "lng": 30.5450267316
+//         },
+//         "animals": true,
+//         "description": "string",
+//         "disabledAccess": true,
+//         "id": 0,
+//         "isActive": true,
+//         "kosher": true,
+//         "mamad": true,
+//         "maxPeople": "danielrogel",
+//         "owner": {
+//           "contactPhone": "danielrogel",
+//           "email": "steve.simonis@yahoo.com",
+//           "name": "danielrogel"
+//         },
+//         "photo": "string"
+// }
+    var toSubmit = {    
+        "address": {
+            "country": "Qatar",
+            "city": "South Desireberg",
+            "street": "Franklyn Parkway",
+            "apt": 5320,
+            "lat": 30.412027,
+            "lng": 30.5457316
+        },
+        "description": "string",
+        "isActive": true,
+        "maxPeople": 2,
+        "owner": {
+          "contactPhone": "333",
+          "email": "steve.simonis55@yahoo.com",
+          "name": "danielrogel"
+        },
+        "photo": "string"
+}
+    var response = await axios.post("http://hashlama005team8.azurewebsites.net/houses",toSubmit).
+    then(()=>{console.log("good push")}).catch((err)=>{console.log("Somthiing went wrong")})
+}
+
 
 function Header () {
 
     const[register, setRegister] = useState(false);
+    const[address, setAddress] = useState();
+    const[num, setNum] = useState();
+    const[name, setName] = useState();
+    const[phone, setPhone] = useState();
 
     return (
         <div>
@@ -18,17 +73,19 @@ function Header () {
             </div>
             {register && 
             <div className={headerStyles.popup}>
+                <form>
                 <div className={headerStyles.item} style={{ top:'10%', fontWeight: 'bold' }}>הוספת דירה</div>
                 <input tag="text" className={headerStyles.item} style={{ top:'30%', left:'35%' }}></input>
-                <div className={headerStyles.item} style={{ top:'30%', left:'70%' }}>כתובת</div>
+                <div className={headerStyles.item} style={{ top:'30%', left:'70%' }} onchange={(e) => { setAddress(e) }}>כתובת</div>
                 <input tag="text" className={headerStyles.item} style={{ top:'30%', left:'35%' }}></input>
-                <div className={headerStyles.item} style={{ top:'50%', left:'70%' }}>מספר מתארחים אפשרי</div>
+                <div className={headerStyles.item} style={{ top:'50%', left:'70%' }} onchange={(e) => { setNum(e) }}>מספר מתארחים אפשרי</div>
                 <input tag="text" className={headerStyles.item} style={{ top:'50%', left:'35%' }}></input>
-                <div className={headerStyles.item} style={{ top:'70%', left:'70%' }}>שם איש הקשר</div>
+                <div className={headerStyles.item} style={{ top:'70%', left:'70%' }} onchange={(e) => { setName(e) }}>שם איש הקשר</div>
                 <input tag="text" className={headerStyles.item} style={{ top:'70%', left:'35%' }}></input>
-                <div className={headerStyles.item} style={{ top:'90%', left:'70%' }}>מספר טלפון</div>
+                <div className={headerStyles.item} style={{ top:'90%', left:'70%' }} onchange={(e) => { setPhone(e) }}>מספר טלפון</div>
                 <input tag="text" className={headerStyles.item} style={{ top:'90%', left:'35%' }}></input>
-                <button className={headerStyles.item} style={{ top:'90%', left:'10%' }}>פרסם</button>
+                <button className={headerStyles.item} style={{ top:'90%', left:'10%' }} onClick={handleSubmit(address,num,name,phone)}>פרסם</button>
+                </form>
             </div>}
 
         </div>
